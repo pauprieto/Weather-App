9ad2dcd67d9f20dce8bedff7c131dd79679c26e8
Paula Prieto <paueprieto@gmail.com>
2023-12-20 15:27:14 +1100

    Added weather API functionality
    - Axios API
    - Displaying current weather data by city input

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
+// Function that displays the current city weather
+function displayWeatherData(response) {
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temp-value");
+
+    let city = response.data.city;
+    let temperature = Math.round(response.data.temperature.current);
+
+    cityElement.innerHTML = `${city}`;
+    temperatureElement.innerHTML = `${temperature}`;
+}
+
+// function using Axios API to get the city weather data
+function getCity(city) {
+    let apiKey = "2b0dc590cd47ao3bt2dcefd407454554";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeatherData);
+}
+
 // Function that converts current City to input text
 function handleSearch(event) {
     event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    let searchInputElement = document.querySelector("#search-input");
+
+    getCity(searchInputElement.value);
 }
 
 // Search button event
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+getCity("Melbourne");
